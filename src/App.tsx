80c745bf4ae57a9f532ad1d9796bc043b27af448
Laoset <xlaospt2@gmail.com>
2023-04-19 18:52:17 -0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import GameCenter from "./games/GameCenter";
-import Memotest from "./games/Memotest";
-import Pokemon from "./games/Pokemon";
-import WordsPerMinute from "./games/wordsPerMinute";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<GameCenter />} />
-      {/* <Route path="/memotest" element={<Memotest />} />
-      <Route path="/pokemon" element={<Pokemon />} />
-      <Route path="/wpm" element={<WordsPerMinute />} /> */}
     </Routes>
   );
 }
--- src/games/GameCenter.tsx
+++ src/games/GameCenter.tsx
@@ -41,11 +41,11 @@ const GameCenter = () => {
         </h1>
       </div>
       <div className="flex justify-center text-center flex-col gap-10">
-        <ul className="flex justify-between text-center flex-row">
+        <ul className="flex justify-between lg:justify-evenly text-center flex-row">
           <li
             className={`text-2xl ${
               selectedGame === "memotest"
-                ? "text-nav text-xl font-fireCode font-semibold md:text-2xl cursor-pointer hover:scale-110 duration-200 italic mx-2 lg:mx-0"
+                ? "text-nav text-[22px]  font-fireCode font-semibold lg:text-3xl cursor-pointer hover:scale-110 duration-200 italic mx-2 lg:mx-0"
                 : "text-white font-fireCode hover:text-nav text-[22px] font-semibold md:text-2xl cursor-pointer hover:scale-110 duration-200 italic mx-2 lg:mx-0"
             }`}
             onClick={() => handleGameSelect("memotest")}
@@ -55,7 +55,7 @@ const GameCenter = () => {
           <li
             className={`text-2xl ${
               selectedGame === "wpm"
-                ? "text-nav  text-[22px] font-fireCode font-semibold md:text-2xl cursor-pointer hover:scale-110 duration-200 italic mx-2 lg:mx-0"
+                ? "text-nav  text-[22px] font-fireCode font-semibold md:text-3xl cursor-pointer hover:scale-110 duration-200 italic mx-2 lg:mx-0"
                 : "text-white font-fireCode text-[22px]  font-semibold md:text-2xl cursor-pointer hover:scale-110 duration-200 italic hover:text-nav mx-2 lg:mx-0"
             }`}
             onClick={() => handleGameSelect("wpm")}
@@ -65,7 +65,7 @@ const GameCenter = () => {
           <li
             className={`text-2xl ${
               selectedGame === "pokemon"
-                ? "text-nav font-fireCode text-[22px] font-semibold md:text-2xl cursor-pointer hover:scale-110 duration-200 italic mx-2 lg:mx-0"
+                ? "text-nav font-fireCode text-[22px] font-semibold md:text-3xl cursor-pointer hover:scale-110 duration-200 italic mx-2 lg:mx-0"
                 : "text-white font-fireCode text-[22px] font-semibold md:text-2xl cursor-pointer hover:scale-110 duration-200 italic hover:text-nav mx-2 lg:mx-0"
             }`}
             onClick={() => handleGameSelect("pokemon")}
